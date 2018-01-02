236e7ff6c7bb6c42603db1755ec52d6c0b92f068
wicked-mind <wickedmind2016@gmail.com>
2018-01-03 01:32:13 +0530

    Added Swagger Functionality.

--- src/main/java/com/bookservice/controller/BookController.java
+++ src/main/java/com/bookservice/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.bookservice.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,11 +16,13 @@ import com.bookservice.service.BookService;
 
 @RestController
 @RequestMapping(value="/bookservices")
+@Api(value="BookService", description="Book Service for Library")
 public class BookController {
 	
 	private BookService bookService;
 	
 	@RequestMapping(value="/getallbooks", method=RequestMethod.GET)
+	@ApiOperation(value = "View a list of books", response = ResultBean.class)
 	public ResultBean getAllBooks() throws Exception {
 		ResultBean resultBean = new ResultBean();
 		HashMap<String, ArrayList<Object>> userResultMap = new HashMap<String, ArrayList<Object>>();
@@ -27,6 +32,7 @@ public class BookController {
 	}
 	
 	@RequestMapping(value="/updatebook", method=RequestMethod.GET)
+	@ApiOperation(value="Update Book in Book List", response=ResultBean.class)
 	public ResultBean updateBook(Book book) throws Exception {
 		bookService.updateBook(book);
 		ResultBean resultBean = new ResultBean();
@@ -34,6 +40,7 @@ public class BookController {
 	}
 
 	@RequestMapping(value="/getbookcount", method=RequestMethod.GET)
+	@ApiOperation(value="Get count of a specific book", response=ResultBean.class)
 	public ResultBean getbookcount(String bookName) throws Exception {
 		int count = bookService.getbookcount(bookName);
 		System.out.println("Book Count: " + count);
@@ -41,6 +48,7 @@ public class BookController {
 		return resultBean;
 	}
 	
+	@ApiOperation(value="Retire a book", response=ResultBean.class)
 	@RequestMapping(value="/retirebook", method=RequestMethod.GET)
 	public ResultBean retirebook(String bookName, String retireDate) throws Exception {
 		/*Date retireDate = new Date(retireDate);
